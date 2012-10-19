a7c8bd894cc89c2fc828d7bf3ce5e20a71901639
KirillBakanov <kirill.bakanov@zojax.com>
2012-10-19 15:59:49 +0300

    fixed blog button

--- src/zojax/project/project_permissions.py
+++ src/zojax/project/project_permissions.py
@@ -3,16 +3,17 @@ from zope.component import getUtility
 from zope.securitypolicy.interfaces import IRolePermissionMap
 
 from zojax.permissionsmap.interfaces import IPermissionsMap
+from zojax.blogger.interfaces import *
 from interfaces import IProject
-
-@component.adapter(IProject)
+@component.adapter(IBlog)
 @interface.implementer(IRolePermissionMap)
 def getContentPermissions(context):
-    if context.ptype == 'open':
-        return getUtility(IPermissionsMap, 'project.open')
-    elif context.ptype == 'members':
-        return getUtility(IPermissionsMap, 'project.members')
-    elif context.ptype == 'private':
-        return getUtility(IPermissionsMap, 'project.private')
-    else:
-        return getUtility(IPermissionsMap, 'project.secret')
+    if IProject.providedBy(context.__parent__):
+        if context.__parent__.ptype == 'open':
+            return getUtility(IPermissionsMap, 'project.open')
+        elif context.__parent__.ptype == 'members':
+            return getUtility(IPermissionsMap, 'project.members')
+        elif context.__parent__.ptype == 'private':
+            return getUtility(IPermissionsMap, 'project.private')
+        else:
+            return getUtility(IPermissionsMap, 'project.secret')
--- src/zojax/project/roles.py
+++ src/zojax/project/roles.py
@@ -7,33 +7,18 @@ from zope.app.security.settings import Allow, Unset, Deny
 from zope.security.proxy import removeSecurityProxy
 from zope.securitypolicy.interfaces import IPrincipalRoleMap
 from zojax.quick.contenttypes.browser.members import TopRole
-from interfaces import IProject
+from zojax.blogger.interfaces import *
 
 
 class ProjectsMembersRoles(object):
-    component.adapts(IProject)
+    component.adapts(IBlog)
     interface.implements(IPrincipalRoleMap)
 
     def __init__(self, context):
         self.context = context
-        self.members = context.members
+        self.members = context.__parent__.members
 
     def getPrincipalsForRole(self, role_id):
-        members = {}
-        print 'getPrincipalsForRole'
-        #        if role_id == 'report.LevelReader':
-        #            return [(member, Allow) for member in self.getMembersWithRole(members, MEMBER_ACCESS_READ).keys()]
-        #        elif role_id == 'report.LevelWriter':
-        #            return [(member, Allow) for member in self.getMembersWithRole(members, MEMBER_ACCESS_WRITE).keys()]
-        #        elif role_id == 'report.TopLevelWriter':
-        #            if self.context.title != 'Top level':
-        #                top_level = removeSecurityProxy(self.context.report.topLevel)
-        #            else:
-        #                top_level = self.context
-        #            for member in top_level.members:
-        #                if top_level.members[member].permission == MEMBER_ACCESS_WRITE:
-        #                    members[member] = 1
-        #            return [(member, Allow) for member in members.keys()]
         return ()
 
 
@@ -44,17 +29,13 @@ class ProjectsMembersRoles(object):
             roles['group.Member'] = 1
             if member not in getattr(self.members, 'managers', []):
                 roles['group.Manager'] = 0
-                print 'Manager = 0'
             else:
                 roles['group.Manager'] = 1
-                print 'ahaha Manager =1'
-
         return [(role, value and Allow or Deny) for role, value in roles.items()]
 
     def getSetting(self, role_id, member):
         return Unset
 
     def getPrincipalsAndRoles(self):
-        print 'getPrincipalsAndRoles'
         pass
 
