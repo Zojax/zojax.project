58f13213090d02ebb84eb1131fae03c39be3ffae
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2012-10-19 17:26:33 +0300

    fixed project_roles

--- src/zojax/project/roles.py
+++ src/zojax/project/roles.py
@@ -9,7 +9,6 @@ from zope.securitypolicy.interfaces import IPrincipalRoleMap
 from zojax.quick.contenttypes.browser.members import TopRole
 from zojax.blogger.interfaces import *
 
-
 class ProjectsMembersRoles(object):
     component.adapts(IBlog)
     interface.implements(IPrincipalRoleMap)
@@ -21,16 +20,15 @@ class ProjectsMembersRoles(object):
     def getPrincipalsForRole(self, role_id):
         return ()
 
-
-
     def getRolesForPrincipal(self, member):
         roles = {'group.Member': 0, 'group.Manager': 0}
-        if member in self.members:
-            roles['group.Member'] = 1
-            if member not in getattr(self.members, 'managers', []):
-                roles['group.Manager'] = 0
-            else:
-                roles['group.Manager'] = 1
+        if self.members:
+            if member in self.members:
+                roles['group.Member'] = 1
+                if member not in getattr(self.members, 'managers', []):
+                    roles['group.Manager'] = 0
+                else:
+                    roles['group.Manager'] = 1
         return [(role, value and Allow or Deny) for role, value in roles.items()]
 
     def getSetting(self, role_id, member):
