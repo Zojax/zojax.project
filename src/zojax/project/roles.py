3d18b799056cc05662d5dc141ae89eb269fc5341
KirillBakanov <kirill.bakanov@zojax.com>
2012-10-19 17:35:04 +0300

    fixed roles py

--- src/zojax/project/roles.py
+++ src/zojax/project/roles.py
@@ -25,12 +25,13 @@ class ProjectsMembersRoles(object):
 
     def getRolesForPrincipal(self, member):
         roles = {'group.Member': 0, 'group.Manager': 0}
-        if member in self.members:
-            roles['group.Member'] = 1
-            if member not in getattr(self.members, 'managers', []):
-                roles['group.Manager'] = 0
-            else:
-                roles['group.Manager'] = 1
+        if self.members:
+            if member in self.members:
+                roles['group.Member'] = 1
+                if member not in getattr(self.members, 'managers', []):
+                    roles['group.Manager'] = 0
+                else:
+                    roles['group.Manager'] = 1
         return [(role, value and Allow or Deny) for role, value in roles.items()]
 
     def getSetting(self, role_id, member):
